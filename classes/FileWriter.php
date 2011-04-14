28e707cb273fa27e6bde45ca38ae32dbfe49e60c
Andrew Kulakov <avk@8xx8.ru>
2011-04-14 23:17:53 +0400

    New Writer class

---
+++ classes/FileWriter.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Cache.php
+ * Класс осуществляющий опрации чтения из файлов.
+ *
+ * Дата создания: 09.04.11
+ *
+ * @author Andrew Kulakov <avk@8xx8.ru>
+ * @version
+ * @copyright Andrew Kulakov (c) 2011
+ */
+require_once("Log.php");
+
+class CI_FileWriter{
+
+    public static function write($filename, $contents){
+
+        $file = fopen($filename, "wt");
+        if ($file !== false){
+            fwrite($file, $contents);
+            fclose($file);
+            return true;
+        } else {
+            CI_Log :: write("Ошибка доступа к файлу ".$filename, "FileWriter.php");
+            die ("Ошибка доступа к файлу ".$filename);
+        }
+    }
+
+    public static function writeGZ($filename, $contents){ 
+        $file = gzopen($filename, "rt");
+        if ($file !== false){
+            gzwrite($file, $contents);
+            fclose($file);
+            return true;
+        } else {
+            CI_Log :: write("Ошибка доступа к файлу ".$filename, "FileReader.php");
+            die ("Ошибка доступа к файлу ".$filename);
+        }
+    }
+}
