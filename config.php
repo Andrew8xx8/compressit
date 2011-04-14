bdc57831d1f14e819e65477d7a30c6373c3fce29
Andrew Kulakov <avk@8xx8.ru>
2011-04-14 23:18:03 +0400

    New PATH options in config

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 return array (
-    'web_path' => '',
+    'web_path' => $_SERVER['SERVER_NAME'],
+    'site_path' => $_SERVER['DOCUMENT_ROOT'],
     'logFile' => '../logs/main_log.txt'
 );
  
