784041bd050084e9d690582df55b364dd31a165f
Andrew Kulakov <avk@8xx8.ru>
2011-04-14 23:59:33 +0400

    Add new optim web_cahce_path

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 return array (
     'web_path' => $_SERVER['SERVER_NAME'],
     'site_path' => $_SERVER['DOCUMENT_ROOT'],
+    'cache_dir' => 'c:\\xampp\\htdocs\\compressit\\test\\cache',
+    'web_cache_dir' => '/cache',
     'logFile' => '../logs/main_log.txt'
 );
  
