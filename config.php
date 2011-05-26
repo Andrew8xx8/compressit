d01b4a6925f9f9d4332302b0c45b1381ae8ee819
Andrew Kulakov <avk@8xx8.ru>
2011-05-26 20:06:30 +0400

    New properties.

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php
 return array (
-    'web_path' => $_SERVER['SERVER_NAME'],
+    'web_path' => "http://".$_SERVER['SERVER_NAME']."/compressit/test",
     'site_path' => $_SERVER['DOCUMENT_ROOT'],
     'cache_dir' => 'c:\\xampp\\htdocs\\compressit\\test\\cache',
-    'web_cache_dir' => '/cache',
-    'logFile' => '../logs/main_log.txt'
+    'web_cache_dir' => "http://".$_SERVER['SERVER_NAME']."/compressit/test/cache",
+    'logFile' => '../logs/main_log.txt',
+	'folder_delimiter' => '\\',
+	'logLevel' => 10
 );
  
